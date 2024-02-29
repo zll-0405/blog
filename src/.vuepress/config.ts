07334a756a0b088e4c9a21bd62a987fb86319dca
zhenglulu <zhenglulu_0405@163.com>
2024-02-29 16:07:52 +0800

    modify base in config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/blog/",
+  // base: "/blog/",
   dest: "src/.vuepress/dist",
   lang: "zh-CN",
   title: "醒醒同学",
