45b05daeac28eb40addf578e6c5177d55a6ebc8c
zhenglulu <zhenglulu_0405@163.com>
2024-02-29 16:03:32 +0800

    add dest config

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import theme from "./theme.js";
 
 export default defineUserConfig({
   base: "/blog/",
-
+  dest: "./dist",
   lang: "zh-CN",
   title: "醒醒同学",
   description: "醒醒同学分享的关于自由职业和教练的那些事",
