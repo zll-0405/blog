ded151d4334780b5ace3f31b5c7cbbee0a0e90e5
zhenglulu <zhenglulu_0405@163.com>
2024-03-28 20:07:19 +0800

    add google analysis

---
+++ src/.vuepress/custom.ts
@@ -0,0 +1,9 @@
+import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
+
+export default {
+  plugins: [
+    googleAnalyticsPlugin({
+      id: "G-8J2XHXFRPY",
+    }),
+  ],
+};
