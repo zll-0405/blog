dd7e79ade7558514ba0725d169d42dba77ea00a1
zhenglulu <zhenglulu_0405@163.com>
2024-03-29 02:24:48 +0800

    add google analysis

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,6 +9,13 @@ export default defineUserConfig({
   description: "醒醒同学分享的关于自由职业和教练的那些事",
   head: [ 
     ['link', { rel: 'icon', href: 'https://vip.helloimg.com/i/2024/03/02/65e2a7e03be1c.jpg' }],
+
+    ['script', { src: "https://www.googletagmanager.com/gtag/js?id=G-8J2XHXFRPY",async: true}],
+    ['script', {},
+                " window.dataLayer = window.dataLayer || [];\
+                function gtag(){dataLayer.push(arguments);}\
+                gtag('js', new Date());\
+                gtag('config', 'G-8J2XHXFRPY');"],
   ],
 
   theme,
--- src/.vuepress/custom.ts
+++
@@ -1,9 +0,0 @@
-import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
-
-export default {
-  plugins: [
-    googleAnalyticsPlugin({
-      id: "G-8J2XHXFRPY",
-    }),
-  ],
-};
